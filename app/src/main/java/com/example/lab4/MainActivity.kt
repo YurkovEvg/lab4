d7dda74e40023f5642d454700640195b81ef700e
YurkovEvg <zhyurkov1@gmail.com>
2024-06-10 13:18:00 +0300

    Start project

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.lab4
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -6,37 +7,73 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans,
+            true),
+        Question(R.string.question_mideast,
+            false),
+        Question(R.string.question_africa,
+            false),
+        Question(R.string.question_americas
+            , true),
+        Question(R.string.question_asia,
+            true))
+    private var currentIndex = 0
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        trueButton =
-            findViewById(R.id.true_button)
-        falseButton =
-            findViewById(R.id.false_button)
-        trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
-        }
-        falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
-        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        trueButton = findViewById(R.id.true_button)
+        trueButton.setOnClickListener{view: View ->
+            checkAnswer(true)
+        }
+
+        falseButton = findViewById(R.id.false_button)
+        falseButton.setOnClickListener{view: View ->
+            checkAnswer(false)
+        }
+
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.textViewQuestion)
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
     }
-}
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer:Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId,
+            Toast.LENGTH_SHORT)
+            .show()
+
+        }
+    }
